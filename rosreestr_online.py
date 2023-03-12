f9be1e29fac539a3bcd4cb86a067e4cc25a30b93
18098 <180982@mail.ru>
2023-03-13 02:18:10 +0700

    Запрос по адресам, результат в txt Запрос по кадастровому номеру, результат в xls

--- rosreestr_online.py
+++ rosreestr_online.py
@@ -4,7 +4,8 @@ import certifi
 import sys
 import locale
 from urllib.request import urlopen
-import ssl
+from openpyxl import load_workbook
+import json
 
 # codirovka = 'C:\Windows\System32\chcp 65001'
 # os.system(codirovka)
@@ -15,33 +16,74 @@ headers = {"User-Agent": "Mozilla / 5.0 (Windows NT 10.0; WOW64) AppleWebKit/537
 		   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 
-url = 'https://rosreestr.gov.ru/fir_lite_rest/api/gkn/fir_object/42:30:0415002:361'
-# url = 'http://rosreestr.ru/api/online/fir_object/42:30:0415002:361'
+# url = 'https://rosreestr.gov.ru/fir_lite_rest/api/gkn/fir_object/42:30:0415002:361'
 
 
-# urlopen(url, context=ssl.create_default_context(cafile=certifi.where()))
-
-# os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.path.dirname(sys.argv[0]), certifi.where())
-# os.environ['REQUESTS_CA_BUNDLE'] = "rosreestr.gov.ru.crt"
-# print(os.environ['REQUESTS_CA_BUNDLE'])
 
 # with requests.get(url, headers=headers, stream=True, timeout=600) as r:
 #     print(r)
 # print(certifi.where())
 
-# param = {'macroRegionId': '140000000000'}
 
 # r = requests.get(url, headers=headers, verify="CertBundle.pem")
 # print(r)
 # print(r.text)
 # print(r.cookies)
 
-r = requests.get(url, headers=headers, verify="CertBundle.pem")
-print(type(r))
-i = 0
-print(r.json())
-for data in r.json():
-	print(data)
-	i += 1
-print(i)
+# r = requests.get(url, headers=headers, verify="CertBundle.pem")
+# print(type(r))
+# i = 0
+# print(r.json())
+# for data in r.json():
+# 	print(data)
+# 	i += 1
+# print(i)
 
+#---------------------поиск по адресу и запись в файл
+# wb = load_workbook('C:/Users/18098/OneDrive/Desktop/Поиск кад номера/list.xlsx')
+# sheet = wb.active
+# for i in range(1, 271):
+# 	ul = sheet[i][4].value
+# 	number = sheet[i][6].value
+# 	url = 'https://rosreestr.ru/fir_lite_rest/api/gkn/address/fir_objects?macroRegionId=132000000000&regionId=132431000000&street='+ str(ul) + '&house=' + str(number)
+# 	print(url)
+# 	r = requests.get(url, headers=headers, verify="CertBundle.pem")
+# 	print(ul, number)
+# 	for a in r.json():
+# 		print(a)
+# 		with open('C:/Users/18098/OneDrive/Desktop/Поиск кад номера/1.txt', 'a') as file:
+# 			for key, value in a.items():
+# 				file.write(f'{key}, {value}')
+# 			file.write('\n')
+
+#---------------------сведения ГКН по кад номеру и запись в файл
+wb = load_workbook('C:/Users/18098/OneDrive/Desktop/Поиск кад номера/list.xlsx')
+sheet: object = wb.active
+for i in range(2, 441):
+	cadnum = sheet[i][2].value
+	url = 'http://rosreestr.ru/fir_lite_rest/api/gkn/fir_object/' + str(cadnum)
+	print(cadnum)
+	r = requests.get(url, headers=headers, verify="CertBundle.pem")
+	# разбираем respons ответ в формате json это словарь. из словаря достаем пару ключа parcelData, а из него rightsReg
+	a = r.json().get("parcelData")
+	rightsReg = a.get("rightsReg")
+	# аналогично из словаря достаем пару ключа objectData, а из него removed (0 на учете, 1 снят с учета)
+	b = r.json().get("objectData")
+	objectType = b.get('objectType')
+	objectName = b.get('objectName')
+	removed = b.get('removed')
+	addressNote = b.get('addressNote')
+	print(objectType)
+	print(objectName)
+	print(addressNote)
+	print(rightsReg)
+	print(removed)
+	# with open('C:/Users/18098/OneDrive/Desktop/Поиск кад номера/parcel.txt', 'a') as file:
+	# 	file.write(r.text)
+	# 	file.write('\n')
+	sheet[i][6].value = objectType
+	sheet[i][7].value = objectName
+	sheet[i][8].value = addressNote
+	sheet[i][9].value = rightsReg
+	sheet[i][10].value = removed
+	wb.save('C:/Users/18098/OneDrive/Desktop/Поиск кад номера/list.xlsx')
